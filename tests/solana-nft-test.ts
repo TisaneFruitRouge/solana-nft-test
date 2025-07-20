99b72d878dcd8f9e5bf65c8d535cce3c036041f7
Vincent WENDLING <vincent.wendling@lilo.org>
2025-07-20 20:21:01 +0200

    initial commit

---
+++ tests/solana-nft-test.ts
@@ -0,0 +1,63 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SolanaNftAnchor } from "../target/types/solana_nft_anchor";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import {
+	findMasterEditionPda,
+	findMetadataPda,
+	mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
+
+describe("solana-nft-anchor", async () => {
+	// Configured the client to use the devnet cluster.
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+	const program = anchor.workspace
+		.SolanaNftAnchor as Program<SolanaNftAnchor>;
+
+	const signer = provider.wallet;
+
+	const umi = createUmi("https://api.devnet.solana.com")
+		.use(walletAdapterIdentity(signer))
+		.use(mplTokenMetadata());
+
+	const mint = anchor.web3.Keypair.generate();
+
+	// derive the metadata account
+	const metadataAccount = findMetadataPda(umi, {
+		mint: publicKey(mint.publicKey),
+	})[0];
+
+	//derive the master edition pda
+	const masterEditionAccount = findMasterEditionPda(umi, {
+		mint: publicKey(mint.publicKey),
+	})[0];
+
+	const metadata = {
+		name: "Kobeni",
+		symbol: "kBN",
+		uri: "https://raw.githubusercontent.com/687c/solana-nft-native-client/main/metadata.json",
+	};
+
+	it("mints nft!", async () => {
+		const tx = await program.methods
+			.initNft(metadata.name, metadata.symbol, metadata.uri)
+			.accounts({
+				signer: provider.publicKey,
+				mint: mint.publicKey,
+				metadataAccount: metadataAccount,
+				masterEditionAccount: masterEditionAccount
+			})
+			.signers([mint])
+			.rpc();
+
+		console.log(
+			`mint nft tx: https://explorer.solana.com/tx/${tx}?cluster=devnet`
+		);
+		console.log(
+			`minted nft: https://explorer.solana.com/address/${mint.publicKey}?cluster=devnet`
+		);
+	});
+});
